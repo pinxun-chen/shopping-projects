7fce371c49793d9ba5607b8854ec853474846c79
pinxun-chen <a930406a@gmail.com>
2025-06-10 15:01:54 +0800

    0610

---
+++ my-project/src/test/java/com/example/demo/Test_AddUser.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import com.example.demo.model.entity.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.util.Hash;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test_AddUser {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void addUser() {
+        String rawPassword = "1234"; // 原始密碼
+        String salt = Hash.getSalt(); // 產生鹽
+        String hash = Hash.getHash(rawPassword, salt); // 加鹽雜湊
+
+        User user = new User();
+        user.setUsername("admin");
+        user.setPasswordHash(hash);
+        user.setSalt(salt);
+        user.setEmail("a930406a@gmail.com");
+        user.setActive(true);
+        user.setRole("ADMIN");
+
+        userRepository.save(user);
+        System.out.println("新增使用者成功！");
+    }
+}
